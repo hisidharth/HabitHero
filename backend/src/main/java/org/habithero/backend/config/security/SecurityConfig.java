3130cb4b5cad1842e6c04f9218d9f486568861e6
Sidharth Krishnaswamy <hisidharth2014@gmail.com>
2025-05-09 16:37:07 -0700

    Update SecurityConfig.java

--- backend/src/main/java/org/habithero/backend/config/security/SecurityConfig.java
+++ backend/src/main/java/org/habithero/backend/config/security/SecurityConfig.java
@@ -8,16 +8,19 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    // Configure the security filter chain
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+    // Define authorization rules
+
                 .authorizeHttpRequests(((authorize) -> authorize
-                        .anyRequest().hasAuthority("SCOPE_habits"))
+                        .anyRequest().hasAuthority("SCOPE_habits")) // Require 'habits' scope for all endpoints
                 )
-                .cors(Customizer.withDefaults())
+                .cors(Customizer.withDefaults()) // Enable default CORS settings
                 .oauth2ResourceServer((oauth2) -> oauth2
-                        .jwt(Customizer.withDefaults()))
+                        .jwt(Customizer.withDefaults())) // Configure JWT authentication
                 .build();
     }
 }
